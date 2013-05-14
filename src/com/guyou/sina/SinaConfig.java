272fd50975e4724f4d350bf7fea027171a5399ab
ZhuXiaolei <childmercy@gmail.com>
2013-05-14 15:37:24 +0800

    deleted unuseless class

---
+++ src/com/guyou/sina/SinaConfig.java
@@ -0,0 +1,28 @@
+package com.guyou.sina;
+
+public class SinaConfig{
+	private final String APP_KEY = "3322971079";
+	
+	//替换为开发者REDIRECT_URL
+	private final String REDIRECT_URL = "http://www.sina.com";
+	    
+	//新支持scope 支持传入多个scope权限，用逗号分隔
+	private final String SCOPE = "email,direct_messages_read,direct_messages_write," +
+			"friendships_groups_read,friendships_groups_write,statuses_to_me_read," +
+				"follow_app_official_microblog";
+
+
+	public String getAPP_KEY() {
+		return APP_KEY;
+	}
+
+
+	public String getREDIRECT_URL() {
+		return REDIRECT_URL;
+	}
+
+
+	public String getSCOPE() {
+		return SCOPE;
+	}
+}
--- src/com/novo/socalintegration/PlatformControl.java
+++ src/com/novo/socalintegration/PlatformControl.java
@@ -1,5 +1,35 @@
 package com.novo.socalintegration;
 
-public class PlatformControl{
+import android.app.Activity;
+import android.util.Log;
+
+import com.guyou.sina.SinaConfig;
+import com.sina.weibo.sdk.WeiboSDK;
+import com.sina.weibo.sdk.api.IWeiboAPI;
+
+public class PlatformControl {
 	
+	private static final String TAG = "PlatformControl";
+	
+	private IWeiboAPI mSinaWeiboAPI;
+
+	private Activity baseActivity;
+
+	public PlatformControl(Activity activity) {
+		baseActivity = activity;
+	}
+
+	private void initWeiboSDK() {
+		mSinaWeiboAPI = WeiboSDK.createWeiboAPI(baseActivity,
+				new SinaConfig().getAPP_KEY());
+	}
+
+	private void reqWeibo() {
+		if (mSinaWeiboAPI != null) {
+			boolean isDone = mSinaWeiboAPI.registerApp();
+			if(isDone){
+				Log.e(TAG, "********Î¢²©×¢²á³É¹¦");
+			}
+		}
+	}	
 }
