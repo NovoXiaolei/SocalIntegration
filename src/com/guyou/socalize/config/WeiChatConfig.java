a232cd80d801d10c6bfc5998a5b208a39a0a33b9
ZhuXiaolei <childmercy@gmail.com>
2013-11-20 16:31:43 +0800

    增加微信 未完成

---
+++ src/com/guyou/socalize/config/WeiChatConfig.java
@@ -0,0 +1,14 @@
+package com.guyou.socalize.config;
+
+public class WeiChatConfig {
+	private final String AppID = "wx241be31279496aca";
+	private final String AppKey = "a24106fca333bb9d9b20aa16f3d21661";
+	
+	public String getAppID() {
+		return AppID;
+	}
+
+	public String getAppKey() {
+		return AppKey;
+	}
+}
