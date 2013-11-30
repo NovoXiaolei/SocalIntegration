3f03a34b8d8bff8e3c3ace7a9550ef0465f9276d
XiaoLei Zhu <childmercy@gmail.com>
2013-11-30 16:07:13 +0800

    add new code

--- src/com/novo/socalintegration/MainActivity.java
+++ src/com/novo/socalintegration/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.weixin:{
 			Intent intent = new Intent(this, WeiChatActivity.class);
 			startActivity(intent);
+			finish();
 			break;
 		}
 		default:
--- src/com/novo/tecent/WeiChatActivity.java
+++ src/com/novo/tecent/WeiChatActivity.java
@@ -9,8 +9,12 @@ import android.widget.Button;
 import com.guyou.socalize.config.WeiChatConfig;
 import com.novo.common.SocialInterface;
 import com.novo.socialintegration.R;
+import com.novo.util.DebugUtil;
 import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.SendMessageToWX;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.tencent.mm.sdk.openapi.WXMediaMessage;
+import com.tencent.mm.sdk.openapi.WXTextObject;
 
 public class WeiChatActivity extends Activity implements OnClickListener, SocialInterface {
 	private final String TAG = "WeiChatActivity";
@@ -39,17 +43,24 @@ public class WeiChatActivity extends Activity implements OnClickListener, Social
 		setContentView(R.layout.weixin_layout);
 		//init button;
 		init();
+		if(initSDK()){
+			DebugUtil.LogError(TAG, "Regist wei chat successfully");
+		}
+		else
+		{
+			DebugUtil.LogError(TAG, "Regist wei chat failed");
+		}
 	}
 
 	@Override
 	public boolean initSDK() {
 		// TODO Auto-generated method stub
 		String strAppId = new WeiChatConfig().getAppID();
-		m_WXApi = WXAPIFactory.createWXAPI(this, strAppId, true);
-		m_WXApi.registerApp(strAppId);
+		m_WXApi = WXAPIFactory.createWXAPI(this, strAppId);
 		
 		if(null!=m_WXApi)
 		{
+			
 			return true;
 		}
 		return false;
@@ -58,7 +69,34 @@ public class WeiChatActivity extends Activity implements OnClickListener, Social
 	@Override
 	public void share(String str) {
 		// TODO Auto-generated method stub
+		m_WXApi.registerApp(new WeiChatConfig().getAppID());
+		String text = str;
+		if (text == null || text.length() == 0) {
+			return;
+		}
 		
+		// 初始化一个WXTextObject对象
+		WXTextObject textObj = new WXTextObject();
+		textObj.text = text;
+
+		// 用WXTextObject对象初始化一个WXMediaMessage对象
+		WXMediaMessage msg = new WXMediaMessage();
+		msg.mediaObject = textObj;
+		// 发送文本类型的消息时，title字段不起作用
+		// msg.title = "Will be ignored";
+		msg.description = text;
+
+		// 构造一个Req
+		SendMessageToWX.Req req = new SendMessageToWX.Req();
+		req.transaction = buildTransaction("text"); // transaction字段用于唯一标识一个请求
+		req.message = msg;
+		
+		// 调用api接口发送数据到微信
+		if(m_WXApi.sendReq(req))
+		{
+			DebugUtil.LogError(TAG, "wei chat req send succefully");
+		}
+		//finish();
 	}
 
 	@Override
@@ -81,6 +119,7 @@ public class WeiChatActivity extends Activity implements OnClickListener, Social
 			break;
 		}
 		case R.id.weixin_text_share_button:{
+			share("这只是测试 不要replay");
 			break;
 		}
 		case R.id.weixin_text_image_share_button:{
@@ -95,6 +134,10 @@ public class WeiChatActivity extends Activity implements OnClickListener, Social
 	public void ssoAuthorize() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private String buildTransaction(final String type) {
+		return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
 	}
 
 }
